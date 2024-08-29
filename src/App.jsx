47b087040db991da31893d5a3a36f28796a6713e
khawarshehzad856 <khawarshehzad856@gmail.com>
2024-08-29 17:12:55 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-
-import { DndProvider } from 'react-dnd'
-import './App.css'
-import { HTML5Backend } from 'react-dnd-html5-backend'
-import Drag from './components/Drag'
+import { DndProvider } from "react-dnd";
+import "./App.css";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import Drag from "./components/Drag";
 
 function App() {
-
   return (
     <>
-    <DndProvider backend={HTML5Backend}>
-
-    <div className="text-center w-screen h-screen">
-      <h1 className='text-[25px] font-bold m-auto'>React Drag and Drop</h1>
-    <Drag/>
-      </div>
-    </DndProvider>
+      <DndProvider backend={HTML5Backend}>
+        <div className="text-center w-screen h-screen">
+          <h1 className="text-[25px] font-bold m-auto">React Drag and Drop</h1>
+          <Drag />
+        </div>
+      </DndProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
